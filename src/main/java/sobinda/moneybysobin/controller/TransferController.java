51bb519f7cb7c6951c387da67b465d2247608e75
Dmitry Remizov <rdk.7777@yahoo.com>
2023-10-14 19:37:54 +0300

    first commit

---
+++ src/main/java/sobinda/moneybysobin/controller/TransferController.java
@@ -0,0 +1,30 @@
+package sobinda.moneybysobin.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import sobinda.moneybysobin.exceptions.InvalidTransactionExceptions;
+import sobinda.moneybysobin.model.CardTransfer;
+import sobinda.moneybysobin.model.Verification;
+import sobinda.moneybysobin.service.TransferService;
+
+@RestController
+public class TransferController {
+    TransferService transferService;
+
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping("/transfer")
+    public String transferMoneyCardToCard(@RequestBody @Validated CardTransfer cardTransfer) throws InvalidTransactionExceptions {
+        //выбрать расширение и что возвращает при переводе с карты на карту
+        return transferService.transferMoneyCardToCard(cardTransfer);
+    }
+
+    @PostMapping("/confirmOperation")
+    public String confirmOperation(@RequestBody @Validated Verification verification) throws InvalidTransactionExceptions {
+        //подтверждение нашей операции после успешной проверки карт
+        //выбрать расширение и что возвращает
+        return transferService.confirmOperation(verification);
+    }
+}
